9037900f3dd597545eadcd25f55f7b96da101e65
kirtan <kirtan12082004@gmail.com>
2024-12-07 09:18:31 +0530

    error solve

--- src/componets/Experiance.jsx
+++ src/componets/Experiance.jsx
@@ -11,32 +11,32 @@ import SpringBoot from "../../public/img/springBoot.jpg";
 function Experiance() {
   const [obj, setobj] = useState([
     {
-      id:1,
+      id: 1,
       img: Html,
       name: "HTML",
     },
     {
-      id:2,
+      id: 2,
       img: Css,
       name: "css",
     },
     {
-      id:3,
+      id: 3,
       img: Javascript,
       name: "Javascript",
     },
     {
-      id:4,
+      id: 4,
       img: Oracle,
       name: "Oracle",
     },
     {
-      id:5,
+      id: 5,
       img: Spring,
       name: "Spring",
     },
     {
-      id:6,
+      id: 6,
       img: SpringBoot,
       name: "Spring Boot",
     },
@@ -49,24 +49,21 @@ function Experiance() {
       <p>I've more than 2 years of experiance in below technologies.</p>
 
       <div className="ex-boot d-flex flex-wrap">
-        {obj.map((ele,index) => {
+        {obj.map((ele, index) => {
           return (
-            <>
-              <div
-                className="ex-icon rounded-circle border shadow border-secondary-subtle p-2 m-3 zoom"
-                key={ele.name+index}
-                style={{ height: "200px", width: "200px" }}
-              >
-                <img
-                  src={ele.img}
-                  className="rounded-circle m-2"
-                  alt="mongoo"
-                  style={{ height: "150px", width: "150px" }}
-                  
-                />
-                <h6 className="text-center">{ele.name}</h6>
-              </div>
-            </>
+            <div
+              className="ex-icon rounded-circle border shadow border-secondary-subtle p-2 m-3 zoom"
+              key={ele.name + index}
+              style={{ height: "200px", width: "200px" }}
+            >
+              <img
+                src={ele.img}
+                className="rounded-circle m-2"
+                alt="mongoo"
+                style={{ height: "150px", width: "150px" }}
+              />
+              <h6 className="text-center">{ele.name}</h6>
+            </div>
           );
         })}
       </div>
--- src/componets/Portfolio.jsx
+++ src/componets/Portfolio.jsx
@@ -9,32 +9,32 @@ import java from "../../public/img/java.png";
 function Portfolio() {
   const [cart, setcart] = useState([
     {
-      id:1,
+      id: 1,
       img: mongoo,
       name: "MongoDB",
     },
     {
-      id:2,
+      id: 2,
       img: express,
       name: "Express",
     },
     {
-      id:3,
+      id: 3,
       img: react,
       name: "ReactJS",
     },
     {
-      id:4,
+      id: 4,
       img: node,
       name: "NodeJS",
     },
     {
-      id:5,
+      id: 5,
       img: css,
       name: "Css",
     },
     {
-      id:6,
+      id: 6,
       img: java,
       name: "Java",
     },
@@ -50,32 +50,28 @@ function Portfolio() {
       <div className="cart d-flex flex-wrap justify-content-between">
         {cart.map((ele, index) => {
           return (
-            <>
-              <div className="card zoom" style={{ width: "18rem" }} key={ele.id}>
-                <img
-                  src={ele.img}
-                  className="rounded-circle border border-secondary-subtle p-2 m-3"
-                  alt="mongoo"
-               
-                  style={{ height: "120px", width: "120px" }}
-                />
-                <div className="card-body">
-                  <h5 className="card-title" style={{ fontWeight: 700 }}>
-                    {ele.name}
-                    
-                  </h5>
-                  <p className="card-text" >
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  </p>
-                  <a href="#" className="btn btn-primary">
-                    video
-                  </a>
-                  <a href="#" className="btn btn-success ms-3" >
-                    Source code
-                  </a>
-                </div>
+            <div className="card zoom" style={{ width: "18rem" }} key={ele.id}>
+              <img
+                src={ele.img}
+                className="rounded-circle border border-secondary-subtle p-2 m-3"
+                alt="mongoo"
+                style={{ height: "120px", width: "120px" }}
+              />
+              <div className="card-body">
+                <h5 className="card-title" style={{ fontWeight: 700 }}>
+                  {ele.name}
+                </h5>
+                <p className="card-text">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                </p>
+                <a href="#" className="btn btn-primary">
+                  video
+                </a>
+                <a href="#" className="btn btn-success ms-3">
+                  Source code
+                </a>
               </div>
-            </>
+            </div>
           );
         })}
       </div>
