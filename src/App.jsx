21c5aa21a51fccdff4d35225ddf4a2b7884cb5e5
kirtan <kirtan12082004@gmail.com>
2024-12-09 09:29:25 +0530

    error

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
-import Navbar from './componets/Navbar'
-import Home from './componets/Home'
-import About from './componets/About'
-import Portfolio from './componets/Portfolio'
-import Experiance from './componets/Experiance'
-import Contact from './componets/Contact'
-import Footer from './componets/Footer'
+import Navbar from './componets/Navbar.jsx'
+import Home from './componets/Home.jsx'
+import About from './componets/About.jsx'
+import Portfolio from './componets/Portfolio.jsx'
+import Experiance from './componets/Experiance.jsx'
+import Contact from './componets/Contact.jsx'
+import Footer from './componets/Footer.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
